a12156f028b1f46bb8f378fee05cd719fdf40235
Bradford Wells <bwells@altalang.com>
2019-12-11 23:02:52 -0700

    Add right prompt of k8s context to fish setup

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,14 @@
+function fish_right_prompt
+  function _k8s_context
+    echo (kubectl config current-context)
+  end
+
+  set -l grey (set_color -o grey)
+
+  set k8s_context (_k8s_context)
+  if [ $k8s_context ]
+    set k8s_context \((echo $k8s_context | sed 's/^\([^\.]*\).*/\1/')\)
+  end
+
+  echo -n -s $grey$k8s_context
+end
