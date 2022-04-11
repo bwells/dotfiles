981a01b38bc198d1b008af131785256589cc2e32
Bradford Wells <bwells@altalang.com>
2022-04-11 10:09:00 -0600

    Remove deprecated "maintained" option in treesitter installs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -139,7 +139,8 @@ require('gitsigns').setup()
 -- Treesitter
 -------------
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", or a list of languages
+  ignore_install = { "phpdoc" },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = {},  -- list of language that will be disabled
