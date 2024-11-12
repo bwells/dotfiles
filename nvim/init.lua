c89be9978585317249193fc416713e61456243a3
Bradford Wells <bwells@altalang.com>
2024-11-11 20:43:08 -0700

    Update nvim lazy install to modern style

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,22 +22,20 @@ vim.g.wordmotion_prefix = "<leader>"
 -- with <leader>b to open buffer search
 vim.cmd([[let g:wordmotion_mappings = { 'b': '<M-b>' }]])
 
-
--- vim.cmd([[colorscheme lunaperche]])
-
--- vim.cmd([[set background=dark]])
--- vim.o.background = 'dark'
-
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
