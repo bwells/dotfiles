eb8d03c933a322110d808a884d6e765c1f7300cc
Bradford Wells <bwells@altalang.com>
2025-07-16 14:45:04 -0400

    Add kubernetes namespace to right side prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -8,12 +8,26 @@ function fish_right_prompt
     end
   end
 
-  set -l grey (set_color -o grey)
-
-  set k8s_context (_k8s_context)
-  if [ $k8s_context ]
-    set k8s_context \((echo $k8s_context | sed 's/^\([^\.]*\).*/\1/')\)
+  function _k8s_namespace
+    if type -q kubectl
+      set namespace (kubectl config view --minify --output 'jsonpath={..namespace}' 2>&1)
+      if test $status -eq 0 -a -n "$namespace" -a "$namespace" != "default"
+        echo $namespace
+      end
+    end
   end
 
-  echo -n -s $grey$k8s_context
+  set -l grey (set_color -o grey)
+  set k8s_context (_k8s_context)
+  set k8s_namespace (_k8s_namespace)
+
+  if [ $k8s_context ]
+    set k8s_context (echo $k8s_context | sed 's/^\([^\.]*\).*/\1/')
+    if [ $k8s_namespace ]
+      set k8s_info "($k8s_context:$k8s_namespace)"
+    else
+      set k8s_info "($k8s_context)"
+    end
+    echo -n -s $grey$k8s_info
+  end
 end
