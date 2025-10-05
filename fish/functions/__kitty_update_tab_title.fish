4eb317da380865d885aa77dca2ba1c64dc2fee9d
Bradford Wells <bwells@altalang.com>
2025-10-04 21:19:52 -0400

    Update project title functions

--- fish/functions/__kitty_update_tab_title.fish
+++ fish/functions/__kitty_update_tab_title.fish
@@ -1,17 +1,7 @@
-function __kitty_update_tab_title --description 'Set kitty tab title from project title (tab only)'
-    # Only proceed inside kitty.
-    if test -z "$KITTY_WINDOW_ID"
-        return
-    end
+function __kitty_update_tab_title --description 'Set kitty TAB title (not window title)'
+    test -n "$KITTY_WINDOW_ID"; or return
 
     set -l title (__project_title)
-
-    # Avoid redundant remote calls.
-    if test "$KITTY_CURRENT_TAB_TITLE" = "$title"
-        return
-    end
-    set -gx KITTY_CURRENT_TAB_TITLE "$title"
-
-    # Set the tab title (do not touch window title).
-    command kitty @ set-tab-title "$title" >/dev/null 2>&1
+    # set the TAB title for THIS tab (running inside it)
+    kitty @ set-tab-title "$title"
 end
--- fish/functions/__project_root.fish
+++ fish/functions/__project_root.fish
@@ -1,38 +1,24 @@
-function __project_root --description 'Find project root; prefer Git superproject when in a submodule'
-    set -l root ""
-
-    # If inside a git submodule, this returns the superproject working tree.
-    set -l super (command git rev-parse --show-superproject-working-tree 2>/dev/null)
-    if test -n "$super"
-        set root "$super"
-    else
-        # Toplevel if in a regular git repo.
-        set -l top (command git rev-parse --show-toplevel 2>/dev/null)
-        if test -n "$top"
-            set root "$top"
-        else
-            # Fallback: walk up for common project markers.
-            set -l dir "$PWD"
-            while test "$dir" != "/"
-                if test -e "$dir/.project-root" \
-                    -o -e "$dir/.git" \
-                    -o -e "$dir/pyproject.toml" \
-                    -o -e "$dir/package.json" \
-                    -o -e "$dir/Cargo.toml" \
-                    -o -e "$dir/go.mod" \
-                    -o -e "$dir/Gemfile"
-                    set root "$dir"
-                    break
-                end
-                set dir (dirname "$dir")
-            end
-
-            # If nothing matched, use current dir.
-            if test -z "$root"
-                set root "$PWD"
-            end
+function __project_root --description 'Find project root; prefer Git superproject'
+    # superproject if inside a submodule
+    command git rev-parse --show-superproject-working-tree >/dev/null 2>&1
+    if test $status -eq 0
+        set -l super (command git rev-parse --show-superproject-working-tree 2>/dev/null)
+        if test -n "$super"
+            echo $super
+            return
         end
     end
 
-    echo $root
+    # regular git toplevel
+    command git rev-parse --show-toplevel >/dev/null 2>&1
+    if test $status -eq 0
+        set -l top (command git rev-parse --show-toplevel 2>/dev/null)
+        if test -n "$top"
+            echo $top
+            return
+        end
+    end
+
+    # fallback: current directory
+    echo $PWD
 end
