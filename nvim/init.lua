57928a2763e52dac1abbedece994010a9a596f7e
Brad Wells <kremlan@mars.local>
2026-03-07 18:20:43 -0700

    Remove treesitter playground from config
    Deprecated in favor of built in :InspectTree and :EditQuery

--- nvim/init.lua
+++ nvim/init.lua
@@ -1188,7 +1188,7 @@ require("lazy").setup({
     build = ':TSUpdate',
     dependencies = {
       { 'nvim-treesitter/nvim-treesitter-textobjects' },
-      { 'nvim-treesitter/playground' },
+      -- { 'nvim-treesitter/playground' },
       {
         "nvim-treesitter/nvim-treesitter-context",
         lazy = false,
@@ -1253,24 +1253,24 @@ require("lazy").setup({
             --   ["dF"] = "@class.outer",
             -- },
           },
-          playground = {
-            enable = true,
-            disable = {},
-            updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-            persist_queries = false, -- Whether the query persists across vim sessions
-            keybindings = {
-              toggle_query_editor = 'o',
-              toggle_hl_groups = 'i',
-              toggle_injected_languages = 't',
-              toggle_anonymous_nodes = 'a',
-              toggle_language_display = 'I',
-              focus_language = 'f',
-              unfocus_language = 'F',
-              update = 'R',
-              goto_node = '<cr>',
-              show_help = '?',
-            },
-          },
+          -- playground = {
+          --   enable = true,
+          --   disable = {},
+          --   updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+          --   persist_queries = false, -- Whether the query persists across vim sessions
+          --   keybindings = {
+          --     toggle_query_editor = 'o',
+          --     toggle_hl_groups = 'i',
+          --     toggle_injected_languages = 't',
+          --     toggle_anonymous_nodes = 'a',
+          --     toggle_language_display = 'I',
+          --     focus_language = 'f',
+          --     unfocus_language = 'F',
+          --     update = 'R',
+          --     goto_node = '<cr>',
+          --     show_help = '?',
+          --   },
+          -- },
           incremental_selection = {
             enable = true,
             keymaps = {
